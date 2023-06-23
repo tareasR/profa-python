5d482b55a1cfdb040bd9719ddfcdfd3d4fc8254c
Paisguzman <137554469+Paisguzman@users.noreply.github.com>
2023-06-23 12:36:19 -0600

    Update Asesino.py
    cambios

--- Asesino.py
+++ Asesino.py
@@ -1,34 +1,35 @@
-#Author 
-from Jugador import Jugador
-
-class Assasin(Jugador):
-    super(self)
-    def __init__(self, nombre, arma, velocidad, resistencia, ataque):
-        self.__nombreA = nombre
-        self.__armaA = arma
-        self.__velocidadA = velocidad
-        self.__resistenciaA = resistencia
-        self.__ataqueA = ataque
-
-    def setArma (self, arma):
-        self.__armaA = arma
-    def get_Arma(self):             
-        return self.__armaA
-
-    def setVelocidad (self, velocidad):
-        self.__velocidadA = velocidad
-    def get_Velocidad(self):             
-        return self.__velocidadA
+#Autor Pablo
+    from Jugador import Jugador
+    from Energia import Energia
     
-    def setResistencia (self, resistencia):
-        self.__resistenciaA = resistencia
-    def get_Resistencia(self):             
-        return self.__ResistenciaA
-    
-    def setAtaque (self, ataque):
-        self.__ataqueA = ataque
-    def get_Ataque(self):             
-        return self.__ataqueA
+    class Assasin(Jugador, Energia):
+        
+        def __init__(self, nombre):
+            super().__init__(nombre)
+            self.arma = 'pistola'
+            self.velocidad = 80
+            self.resistencia = 50
+            self.ataque = 70
+            
+        def clase(self):
+            print ('Clase' , type(self), self.nombreX)
+
+        def estadistica(self):
+            print('velocidad: ', self.velocidad)
+            print('resistencia: ', self.resistencia)
+            print('ataque: ', self.ataque)
+            print('vida: ', super().get_vida())
+            print('energia: ', self.get_energia())
+
+        
+        def defender(self):
+            super().set.vida()
+        
+        def atacar (self):
+            super().set_energia()
+            
+        def get_resistencia(self):             
+            return self.resitencia
 
       
             
